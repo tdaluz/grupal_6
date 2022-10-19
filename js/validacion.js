dccce6399bfd37064e33e89aabd598435fb64ea7
Tahiana Daluz <tahiana.daluz@gmail.com>
2022-10-18 22:21:50 -0300

    22:21

--- js/validacion.js
+++ js/validacion.js
@@ -12,13 +12,13 @@ function validData(dato) {
     if (dato.value.length == 0) {
         dato.classList.add("is-invalid")
     }
-    else {
+    else { 
         dato.classList.add("is-valid")
     }
 }
 
 function validPassword() {
-    
+
     if (pass1.value.length < 6) {
         pass1.classList.add("is-invalid")
     }
@@ -26,17 +26,17 @@ function validPassword() {
         pass1.classList.add("is-valid")
     }
 
-        if (pass2.value == pass1.value && pass1.value.length >= 6) {
-            pass2.classList.add("is-valid")
-        }
-        else {
-            pass2.classList.add("is-invalid")
-        }
-    
+    if (pass2.value == pass1.value && pass1.value.length >= 6) {
+        pass2.classList.add("is-valid")
+    }
+    else {
+        pass2.classList.add("is-invalid")
+    }
+
 }
-/////
-function validCheck(){
-    if (check.checked == false){
+
+function validCheck() {
+    if (check.checked == false) {
         btncheck.classList.add("is-invalid")
         btncheck.style.color = "red"
         check.classList.add("is-invalid")
@@ -44,13 +44,12 @@ function validCheck(){
     else {
         btncheck.classList.add("is-valid")
         check.classList.add("is-valid")
+        btncheck.style.color = "green"
     }
 }
 
 function validEmail() {
-
     var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-
     if (email.value.length > 0 && validEmail.test(email.value)) {
         email.classList.add("is-valid")
     }
@@ -59,51 +58,52 @@ function validEmail() {
     }
 
 }
-/////////
-button.addEventListener("click", () => {
-    validData(nombre);
-    validData(apellido);
-    validEmail()
-    validPassword()
-    validCheck();
 
-
-    nombre.addEventListener("input", () => {
-        nombre.classList.remove("is-valid")
-        nombre.classList.remove("is-invalid")
-        validData(nombre);
+function validaDataInput(input) {
+    validData(input);
+    input.addEventListener("input", () => {
+        input.classList.remove("is-valid")
+        input.classList.remove("is-invalid")
+        validData(input);
     });
+}
 
-    apellido.addEventListener("input", () => {
-        apellido.classList.remove("is-valid")
-        apellido.classList.remove("is-invalid")
-        validData(apellido);
-    });
-
+function validaEmailInput() {
+    validEmail();
     email.addEventListener("input", () => {
         email.classList.remove("is-valid")
         email.classList.remove("is-invalid")
-        validEmail()
+        validEmail();
     });
+}
 
-    pass1.addEventListener("input", () => {
-        pass1.classList.remove("is-invalid")
-        pass1.classList.remove("is-valid")
+function validaPassInput(passw) {
+    validPassword();
+    passw.addEventListener("input", () => {
+        passw.classList.remove("is-invalid")
+        passw.classList.remove("is-valid")
         validPassword();
     });
+}
 
-    pass2.addEventListener("input", () => {
-        pass2.classList.remove("is-invalid")
-        pass2.classList.remove("is-valid")
-        validPassword();
-    });
-
-    btncheck.addEventListener("change", () => {  
+function validaBtnCheck() {
+    validCheck()
+    check.addEventListener("change", () => {  
+        check.classList.remove("is-invalid")
+        check.classList.remove("is-valid")
         btncheck.classList.remove("is-invalid")
         btncheck.classList.remove("is-valid")
-        validCheck();
+        validCheck()
     });
-   
+}
+
+button.addEventListener("click", () => {
+    validaDataInput(nombre);
+    validaDataInput(apellido);
+    validaEmailInput();
+    validaPassInput(pass1);
+    validaPassInput(pass2);
+    validaBtnCheck();
 
 });
 
